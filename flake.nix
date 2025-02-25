f07c11f023c19a43139db94dc010e04d5cbccb4a
Jan Novotny <jnovotny@elektroline.cz>
2025-02-25 23:21:07 +0100

    first setting project
    Add flake and create init hugo project

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Personal website for Chris Portela";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+    hugo-coder = {
+      url = "github:luizdepra/hugo-coder";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, utils, ... }:
+    utils.lib.eachSystem [
+      utils.lib.system.x86_64-darwin
+      utils.lib.system.x86_64-linux
+      utils.lib.system.aarch64-darwin
+      utils.lib.system.aarch64-linux
+    ]
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        rec {
+
+          packages.website = pkgs.stdenv.mkDerivation {
+            name = "website";
+            src = self;
+            buildInputs = [ pkgs.git pkgs.nodePackages.prettier ];
+            buildPhase = ''
+              mkdir -p themes
+              ln -s ${inputs.hugo-coder} themes/hugo-coder
+              sed -i -e 's/enableGitInfo = true/enableGitInfo = false/' config.toml
+              ${pkgs.hugo}/bin/hugo
+              ${pkgs.nodePackages.prettier}/bin/prettier -w public '!**/*.{js,css}'
+            '';
+            installPhase = "cp -r public $out";
+          };
+
+          defaultPackage = self.packages.${system}.website;
+
+          apps = rec {
+            hugo = utils.lib.mkApp { drv = pkgs.hugo; };
+            default = hugo;
+          };
+
+          devShell =
+            pkgs.mkShell { buildInputs = [ pkgs.nixpkgs-fmt pkgs.hugo ]; };
+        });
+}
